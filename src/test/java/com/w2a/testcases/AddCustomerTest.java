3fc1b46f07f97250fa7b42352ed0e5197b9da977
vidyasagar <vidyasagar2702@gmail.com>
2019-08-22 01:08:48 +0530

    branch comment

--- src/test/java/com/w2a/testcases/AddCustomerTest.java
+++ src/test/java/com/w2a/testcases/AddCustomerTest.java
@@ -29,6 +29,8 @@ public class AddCustomerTest extends TestBase {
 		type("postcode_CSS",data.get("postcode"));
 		click("addbtn_CSS");
 		Thread.sleep(2000);
+		
+		//branch commit
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 		
 		Assert.assertTrue(alert.getText().contains(data.get("alerttext")));
